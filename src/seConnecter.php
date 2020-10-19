f1eec9195c59d0f0cdd599b40a6ccc69e7ad06ba
Harsisis <cadetgauthier3@gmail.com>
2020-10-19 16:59:48 +0200

    added files

---
+++ src/seConnecter.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link type="text/css" href="css/styles.css">
+    <title>Se Connecter</title>
+</head>
+<body>
+
+<h2>Se Connecter</h2>
+
+<form method="post">
+    <table>
+        <tr>
+            <td>
+                <input type="text" name="nu" value="nom d'utilisateur">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="password" name="mdp" value="mot de passe">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="submit" value="valider" name="Valider">
+            </td>
+        </tr>
+    </table>
+</form>
+
+</body>
+</html>
