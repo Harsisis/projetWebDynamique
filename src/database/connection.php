ebf767ff14e8aad020cfbcfb68c437c2e3a366b1
Harsisis <56965488+Harsisis@users.noreply.github.com>
2020-10-23 15:41:16 +0200

    display all the articles ordered by date

--- src/database/connection.php
+++ src/database/connection.php
@@ -2,7 +2,6 @@
 function connect (){
     try {
         $objPdo = new PDO ('mysql:host=devbdd.iutmetz.univ-lorraine.fr;port=3306;dbname=cadet25u_projetPHP', 'cadet25u_appli', 'Gauthier541609' );
-        echo "connexion ok<br/>\n";
     }
     catch( Exception $exception ) {
         die($exception->getMessage());
